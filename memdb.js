a2026241c7a283c850e74c2339a505ba22a98f6d
Anwesh43 <anweshthecool0@gmail.com>
2020-02-28 21:57:10 +0530

    adding InMemoryDb class

---
+++ memdb.js
@@ -0,0 +1,55 @@
+class InMemoryDb {
+
+    constructor() {
+        this.dataMap = new Map()
+        this.listDataMap = new Map()
+        this.setDataMap = new Map()
+    }
+
+    put(key, value) {
+        this.dataMap.set(key, value)
+    }
+
+    get(key) {
+        if (this.dataMap.has(key)) {
+            return this.dataMap.get(key)
+        } else {
+            throw new Error("key doesn't exist")
+        }
+    }
+
+    ladd(key, value) {
+        if (!this.listDataMap.has(key)) {
+            this.listDataMap.set(key, [])
+        }
+        this.listDataMap.get(key).push(value)
+    }
+
+    lgetall(key) {
+        if (this.listDataMap.has(key)) {
+            return this.listDataMap.get(key)
+        } else {
+            throw new Error("key doesn't exist")
+        }
+    }
+
+    sadd(key, value) {
+        if (!this.setDataMap.has(key)) {
+            this.setDataMap.set(key, new Set())
+        } else {
+            this.setDataMap.get(key).add(10)
+        }
+    }
+
+    sgetall(key) {
+      if (this.setDataMap.has(key)) {
+          return this.setDataMap.get(key)
+      } else {
+          throw new Error("key doesn't exist")
+      }
+    }
+}
+
+const inMemoryDb = new InMemoryDb()
+
+module.exports = inMemoryDb
